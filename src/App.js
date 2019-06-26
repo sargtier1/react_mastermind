8c7c0951ffd665143cc4c2b7d8a549c82e521237
JustTams <sarg.tier1@gmail.com>
2019-06-26 12:40:36 -0500

    added color picker component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Header from './components/header'
 import GameBoard from './components/gameBoard'
+import ColorPicker from './components/colorPicker'
 import Footer from './components/footer'
 
 import './App.css'
@@ -12,6 +13,7 @@ export default class App extends Component {
       <div className='App'>
         <Header />
         <GameBoard />
+        <ColorPicker />
         <Footer />
       </div>
     )
