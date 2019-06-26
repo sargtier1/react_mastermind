8c84bebf77cf8e8f05cf9adcf76247d1542b2b08
JustTams <sarg.tier1@gmail.com>
2019-06-26 12:26:08 -0500

    made gameboard

---
+++ src/components/gameBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GameBoard = () => {
+  return (
+    <>
+      <h1>Gameboard</h1>
+      <p>This is the game board</p>
+    </>
+  )
+}
+
+export default GameBoard
