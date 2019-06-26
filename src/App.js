d1c016d7ae3af6da88272b266d1a5cccf21156b5
JustTams <sarg.tier1@gmail.com>
2019-06-26 12:26:41 -0500

    import gameboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+
+import Header from './components/header'
+import GameBoard from './components/gameBoard'
+import Footer from './components/footer'
+
 import './App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>Massa Mind</header>
+        <Header />
+        <GameBoard />
+        <Footer />
       </div>
     )
   }
