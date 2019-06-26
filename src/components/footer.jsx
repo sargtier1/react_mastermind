39724db748d7d9ff7914220ddf9de8abb024e804
JustTams <sarg.tier1@gmail.com>
2019-06-26 12:25:47 -0500

    created a footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const Footer = () => {
+  return <StyledFooter>This was made with love + react</StyledFooter>
+}
+
+const StyledFooter = styled.footer`
+ 
+`
+
+export default Footer
