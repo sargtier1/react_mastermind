8c430ee171499353949077858268f3a9cb9aed6a
JustTams <sarg.tier1@gmail.com>
2019-06-26 12:51:16 -0500

    flushed out guess peg

---
+++ src/components/guessPeg.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function GuessPeg() {
+  return (
+    <GuessPegComponent>
+      <h1>Guess Peg (singular)</h1>
+    </GuessPegComponent>
+  )
+}
+
+const GuessPegComponent = styled.div``
