d6a344c902898b8eb90d1e0d3748e09369b9845a
JustTams <sarg.tier1@gmail.com>
2019-07-01 04:16:45 -0500

    mastermind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,143 @@
 import React, { Component } from 'react'
-
-import Header from './components/header'
-import GameBoard from './components/gameBoard'
-
-import ColorPicker from './components/colorPicker'
-import GameTime from './components/gameTime'
-import NewGameBtn from './components/newGameBtn'
-
-import Footer from './components/footer'
-
 import './App.css'
+import GameBoard from './components/GameBoard/GameBoard'
+import ColorPicker from './components/ColorPicker/ColorPicker'
+import GameTimer from './components/GameTimer/GameTimer'
+import NewGameButton from './components/NewGameButton/NewGameButton'
 
-export default class App extends Component {
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD']
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = this.getInitialState()
+  }
+
+  getInitialState() {
+    return {
+      selColorIdx: 0,
+      guesses: [this.getNewGuess()],
+      code: this.genCode()
+    }
+  }
+
+  getNewGuess() {
+    return {
+      code: [null, null, null, null],
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    }
+  }
+
+  genCode() {
+    return new Array(4).fill().map(dummy => Math.floor(Math.random() * 4))
+  }
+
+  getWinTries() {
+    let lastGuess = this.state.guesses.length - 1
+    return this.state.guesses[lastGuess].score.perfect === 4 ? lastGuess + 1 : 0
+  }
+
+  handleColorSelection = colorIdx => {
+    this.setState({ selColorIdx: colorIdx })
+  }
+
+  handleNewGameClick = () => {
+    this.setState(this.getInitialState())
+  }
+
+  handlePegClick = pegIdx => {
+    let currentGuessIdx = this.state.guesses.length - 1
+
+    let guessesCopy = [...this.state.guesses]
+    let guessCopy = { ...guessesCopy[currentGuessIdx] }
+    let codeCopy = [...guessCopy.code]
+
+    codeCopy[pegIdx] = this.state.selColorIdx
+
+    guessCopy.code = codeCopy
+
+    guessesCopy[currentGuessIdx] = guessCopy
+
+    this.setState({
+      guesses: guessesCopy
+    })
+  }
+
+  handleScoreClick = () => {
+    let currentGuessIdx = this.state.guesses.length - 1
+    let guessCodeCopy = [...this.state.guesses[currentGuessIdx].code]
+    let secretCodeCopy = [...this.state.code]
+    let perfect = 0,
+      almost = 0
+
+    guessCodeCopy.forEach((code, idx) => {
+      if (secretCodeCopy[idx] === code) {
+        perfect++
+    
+        guessCodeCopy[idx] = secretCodeCopy[idx] = null
+      }
+    })
+
+    guessCodeCopy.forEach((code, idx) => {
+      if (code === null) return
+      let foundIdx = secretCodeCopy.indexOf(code)
+      if (foundIdx > -1) {
+        almost++
+        secretCodeCopy[foundIdx] = null
+      }
+    })
+
+    let guessesCopy = [...this.state.guesses]
+    let guessCopy = { ...guessesCopy[currentGuessIdx] }
+    let scoreCopy = { ...guessCopy.score }
+
+    scoreCopy.perfect = perfect
+    scoreCopy.almost = almost
+
+    guessCopy.score = scoreCopy
+
+    guessesCopy[currentGuessIdx] = guessCopy
+
+    if (perfect !== 4) guessesCopy.push(this.getNewGuess())
+
+    this.setState({
+      guesses: guessesCopy
+    })
+  }
+
   render() {
+    let winTries = this.getWinTries()
     return (
       <div className='App'>
-        <Header />
-        <GameBoard />
-        <ColorPicker />
-        <GameTime />
-        <NewGameBtn />
-        <Footer />
+        <header className='App-header-footer'>
+          R E A C T &nbsp;&nbsp;&nbsp; M A S T E R M I N D
+        </header>
+        <div className='flex-h align-flex-end'>
+          <GameBoard
+            colors={colors}
+            guesses={this.state.guesses}
+            handlePegClick={this.handlePegClick}
+            handleScoreClick={this.handleScoreClick}
+          />
+          <div className='App-controls'>
+            <ColorPicker
+              colors={colors}
+              selColorIdx={this.state.selColorIdx}
+              handleColorSelection={this.handleColorSelection}
+            />
+            <GameTimer />
+            <NewGameButton handleNewGameClick={this.handleNewGameClick} />
+          </div>
+        </div>
+        <footer className='App-header-footer'>
+          {winTries ? `You Won in ${winTries} Guesses!` : 'Good Luck!'}
+        </footer>
       </div>
     )
   }
 }
+
+export default App
--- src/components/header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return <header className='App-header'>Massa Mind</header>
-}
-
-export default Header
